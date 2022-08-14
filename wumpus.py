cab099cfba55b006c8efd7230418d66db0337150
Robert Colson <robjohncolson@protonmail.com>
2022-08-14 15:27:08 -0500

    added a comment to wumpus

--- wumpus.py
+++ wumpus.py
@@ -1,1 +1,7 @@
-print "hello!"
+from reloading import reloading
+from random import choice
+
+cave_numbers = range(1,21)
+wumpus_location = choice(cave_numbers)
+# This is a comment, for comment's sake.
+
